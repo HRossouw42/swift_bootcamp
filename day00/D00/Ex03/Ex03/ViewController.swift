3b0f491986e961f7b7689c2358cb641aca869f14
Harmun <hrossouw@student.wethinkcode.co.za>
2019-10-08 16:03:34 +0200

    basic calculator up

--- day00/D00/Ex03/Ex03/ViewController.swift
+++ day00/D00/Ex03/Ex03/ViewController.swift
@@ -11,13 +11,83 @@ import UIKit
 class ViewController: UIViewController {
     
     var numberOnScreen: Double = 0
+    var inputA: Double = 0
+    var inputB: Double = 0
+    var isMath: Bool = false
+    var symbol = 0
     
     @IBOutlet weak var outputLabel: UILabel!
     
     @IBAction func numbers(_ sender: UIButton) {
-        outputLabel.text = outputLabel.text! + String(sender.tag-1)
-        numberOnScreen = Double(outputLabel.text!)!
+        if (isMath == true){
+            outputLabel.text = String(sender.tag-1)
+            numberOnScreen = Double(outputLabel.text!)!
+            isMath = false
+        }
+        else {
+            outputLabel.text = outputLabel.text! + String(sender.tag-1)
+            numberOnScreen = Double(outputLabel.text!)!
+        }
+        //inputA = numberOnScreen
     }
+    
+    @IBAction func functionButtons(_ sender: UIButton) {
+        if outputLabel.text != "" && sender.tag != 12 && sender.tag != 17 && sender.tag != 11 {
+            isMath = true
+            inputA = Double(outputLabel.text!)!
+            // divide
+            if sender.tag == 13 {
+                outputLabel.text = "/"
+            }
+            // multiply
+            else if sender.tag == 14 {
+                outputLabel.text = "*"
+            }
+            // subtract
+            else if sender.tag == 15 {
+                outputLabel.text = "-"
+            }
+            // addition
+            else if sender.tag == 16 {
+                outputLabel.text = "+"
+            }
+            
+            symbol = sender.tag
+        }
+        // if '=' is pressed
+        else if sender.tag == 17 {
+            // divide
+            if symbol == 13 {
+                outputLabel.text = String(inputA / numberOnScreen)
+            }
+            // multiply
+            else if symbol == 14 {
+                outputLabel.text = String(inputA * numberOnScreen)
+            }
+            // subtract
+            else if symbol == 15 {
+                outputLabel.text = String(inputA - numberOnScreen)
+            }
+            // addition
+            else if symbol == 16 {
+                outputLabel.text = String(inputA + numberOnScreen)
+            }
+        }
+        // AC resets everything
+        else if sender.tag == 12 {
+            outputLabel.text = ""
+            numberOnScreen = 0
+            inputA = 0
+            symbol = 0
+        }
+        else if sender.tag == 11 &&
+            outputLabel.text != ""{
+            print("REEEEEEE")
+            outputLabel.text = String(Double(outputLabel.text!)! * -1)
+            print(outputLabel.text!)
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
