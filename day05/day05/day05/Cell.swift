e3765d136104809b1a983120a79f890d25b49ad4
Harmun <hrossouw@student.wethinkcode.co.za>
2019-10-14 15:25:07 +0200

    ex01

---
+++ day05/day05/day05/Cell.swift
@@ -0,0 +1,41 @@
+//
+//  Cell.swift
+//  day05
+//
+//  Created by Harmun Rossouw on 2019/10/14.
+//  Copyright © 2019 Harmun Rossouw. All rights reserved.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+    
+    @IBOutlet weak var cellName: UILabel!
+    
+//    var name: String? = ""
+    
+    override var description: String {
+        return "Cell: \(String(describing: self.cellName.text))"
+    }
+    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+    
+    func setCell(name : String) {
+        cellName.text = name
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- day05/day05/day05/FirstViewController.swift
+++ day05/day05/day05/FirstViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var locations: [String] = ["42", "WeThinkCode", "Google Office"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+}
+    
+}
+
+extension FirstViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locations.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let locationTitle = locations[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! Cell
+        
+        cell.setCell(name: locations[indexPath.row])
+        
+        return cell
+    }
+    
+    
 }
 
