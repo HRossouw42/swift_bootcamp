d559e1e76f9aa72f08e1372da41636718c4b0dfc
Harmun <hrossouw@student.wethinkcode.co.za>
2019-10-14 12:47:33 +0200

    ex00

---
+++ day05/day05/day05/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  day05
+//
+//  Created by Harmun Rossouw on 2019/10/14.
+//  Copyright © 2019 Harmun Rossouw. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
