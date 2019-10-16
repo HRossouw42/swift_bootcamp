67ec32dae21290a19c3d481d285256808599df40
Harmun <hrossouw@student.wethinkcode.co.za>
2019-10-16 16:54:58 +0200

    gestures work with animations

--- day06/day06/day06/ShapeView.swift
+++ day06/day06/day06/ShapeView.swift
@@ -13,51 +13,73 @@ class ShapeView: UIView {
     // Shapes tutorial
     //https://www.weheartswift.com/bezier-paths-gesture-recognizers/
     
-    let size: CGFloat = 150.0
+    // Gravity
+    //https://www.raywenderlich.com/2326-uikit-dynamics-tutorial-getting-started
+    
+    let size: CGFloat = 100.0
     let lineWidth: CGFloat = 3
     var fillColor: UIColor!
     
     //for random shapes
     var path: UIBezierPath!
     
+    //ANIMATION
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    var collision: UICollisionBehavior!
+    var behaviour: UIDynamicItemBehavior!
+    var originalBounds: CGRect!
+    
     func randomColor() -> UIColor {
         let hue:CGFloat = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
         return UIColor(hue: hue, saturation: 0.8, brightness: 1.0, alpha: 0.8)
     }
-    
-    init(origin: CGPoint) {
-        super.init(frame: CGRect(x: 0.0, y: 0.0, width: size, height: size))
-        self.path = randomPath()
-        self.fillColor = randomColor()
-        self.backgroundColor = UIColor.clear
-        self.center = origin
         
+    init(origin: CGPoint, animator: UIDynamicAnimator, gravity: UIGravityBehavior, collision: UICollisionBehavior, behaviour: UIDynamicItemBehavior){
+        super.init(frame: CGRect(x: 0.0, y: 0.0, width: size, height: size))
+        self.center = origin
+        self.backgroundColor = UIColor.clear
+        self.fillColor = randomColor()
+        
+        //shape
+        self.path = randomPath()
+        
+        //animations
+        self.animator = animator
+        self.gravity = gravity
+        self.collision = collision
+        self.behaviour = behaviour
+        
+        self.originalBounds = CGRect(x: 0.0, y: 0.0, width: size, height: size)
+        
+        //register gestures
         initGestureRecognizers()
     }
     
     //DRAW FUNCTION
     override func draw(_ rect: CGRect) {
-        UIColor.red.setFill()
+        print("DO DRAW?")
         self.fillColor.setFill()
-        path.fill()
+        self.path.fill()
         
-                
-        path.lineWidth = self.lineWidth
+        self.path.lineWidth = self.lineWidth
         UIColor.black.setStroke()
-        path.stroke()
+        self.path.stroke()
+        print("I DID A DRAW")
     }
     
     //CHOOSE RANDOM SHAPE
     func randomPath() -> UIBezierPath {
-
+        
         let insetRect = self.bounds.insetBy(dx: lineWidth,dy: lineWidth)
-
+        
         let shapeType = arc4random() % 3
-
+        print("Shape type -> \(shapeType)")
+        
         if shapeType == 0 {
             return UIBezierPath(roundedRect: insetRect, cornerRadius: 10.0)
         }
-
+        
         if shapeType == 1 {
             return UIBezierPath(ovalIn: insetRect)
         }
@@ -67,70 +89,99 @@ class ShapeView: UIView {
     //DRAW TRIANGLE
     func trianglePathInRect(rect:CGRect) -> UIBezierPath {
         let path = UIBezierPath()
-
+        
         path.move(to: CGPoint(x: rect.width / 2.0, y: rect.origin.y))
         path.addLine(to: CGPoint(x: rect.width,y: rect.height))
         path.addLine(to: CGPoint(x: rect.origin.x,y: rect.height))
         path.close()
-
+        
         return path
     }
     
     //TOUCH FUNCTIONS
     func initGestureRecognizers() {
-        let panGR = UIPanGestureRecognizer(target: self, action: #selector(ShapeView.didPan(panGR:)))
+        let panGR = UIPanGestureRecognizer(target: self, action: #selector(ShapeView.didPan))
         addGestureRecognizer(panGR)
         
-        let pinchGR = UIPinchGestureRecognizer(target: self, action: #selector(ShapeView.didPinch(pinchGR:)))
+        let pinchGR = UIPinchGestureRecognizer(target: self, action: #selector(ShapeView.didPinch))
         addGestureRecognizer(pinchGR)
         
-        let rotationGR = UIRotationGestureRecognizer(target: self, action: #selector(ShapeView.didRotate(rotationGR:)))
+        let rotationGR = UIRotationGestureRecognizer(target: self, action: #selector(ShapeView.didRotate))
         addGestureRecognizer(rotationGR)
     }
     
     //PAN
     @objc func didPan(panGR: UIPanGestureRecognizer) {
-
-        self.superview!.bringSubviewToFront(self)
-
-        var translation = panGR.translation(in: self)
-
-        
-        //reset center after rotate
-        translation = translation.applying(self.transform)
-        
-        self.center.x += translation.x
-        self.center.y += translation.y
-        
-        panGR.setTranslation(CGPoint.zero, in: self)
+        switch panGR.state {
+            
+        case .began:
+            self.gravity?.removeItem(panGR.view!);
+            
+        case .changed:
+            panGR.view?.center = panGR.location(in: self.superview);
+            self.animator?.updateItem(usingCurrentState: panGR.view!);
+            
+        case .ended:
+            self.gravity?.addItem(panGR.view!);
+            
+        default:
+            break ;
+        }
     }
     
     //PINCH
     @objc func didPinch(pinchGR: UIPinchGestureRecognizer) {
-
-        self.superview!.bringSubviewToFront(self)
-
-        let scale = pinchGR.scale
-
-        self.transform = self.transform.scaledBy(x: scale, y: scale)
-
-        pinchGR.scale = 1.0
+        let view = pinchGR.view as! ShapeView;
+        
+        switch pinchGR.state {
+            
+        case .began:
+            self.gravity?.removeItem(view);
+            
+        case .changed:
+            self.collision.removeItem(view);
+            self.behaviour.removeItem(view);
+            view.bounds.size.width = view.originalBounds.width * pinchGR.scale;
+            view.bounds.size.height = view.originalBounds.height * pinchGR.scale;
+            self.collision.addItem(view);
+            self.behaviour.addItem(view);
+            self.animator?.updateItem(usingCurrentState: view);
+            
+        case .ended:
+            view.originalBounds = view.bounds;
+            self.gravity?.addItem(view);
+        default:
+            break ;
+        }
     }
     
     //ROTATE
     @objc func didRotate(rotationGR: UIRotationGestureRecognizer) {
-
-        self.superview!.bringSubviewToFront(self)
-
-        let rotation = rotationGR.rotation
-
-        self.transform = CGAffineTransform(rotationAngle: rotation)
-        //rotationGR.rotation = 0.0
+        switch rotationGR.state {
+            
+        case .began:
+            self.gravity.removeItem(rotationGR.view!);
+            
+        case .changed:
+            self.collision.removeItem(rotationGR.view!);
+            self.behaviour.removeItem(rotationGR.view!);
+            rotationGR.view!.transform = rotationGR.view!.transform.rotated(by: rotationGR.rotation);
+            rotationGR.rotation = 0;
+            self.collision.addItem(rotationGR.view!);
+            self.behaviour.addItem(rotationGR.view!);
+            self.animator?.updateItem(usingCurrentState: rotationGR.view!);
+            
+        case .ended:
+            self.gravity?.addItem(rotationGR.view!);
+            
+        default:
+            break ;
+        }
     }
     
     // We need to implement init(coder) to avoid compilation errors
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-        
+    
 }
--- day06/day06/day06/ViewController.swift
+++ day06/day06/day06/ViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-        
+    
     var animator: UIDynamicAnimator = UIDynamicAnimator()
     var gravity: UIGravityBehavior = UIGravityBehavior()
     var collision: UICollisionBehavior = UICollisionBehavior()
+    let behaviour = UIDynamicItemBehavior()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,28 +21,28 @@ class ViewController: UIViewController {
         let tapGR = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap))
         self.view.addGestureRecognizer(tapGR)
         
-        
+        animator = UIDynamicAnimator(referenceView: self.view)
         collision.translatesReferenceBoundsIntoBoundary = true
+        
+        gravity.gravityDirection = CGVector(dx: 0.0, dy: 0.8)
+        
+        animator.addBehavior(gravity)
         animator.addBehavior(collision)
+        behaviour.elasticity = 0.8
+        animator.addBehavior(behaviour)
     }
     
     @objc func didTap(tapGR: UITapGestureRecognizer) {
+        print("Tippy Tap!")
         let tapPoint = tapGR.location(in: self.view)
         
-        let shapeView = ShapeView(origin: tapPoint)
+        let shapeView = ShapeView(origin: tapPoint, animator: animator, gravity: gravity, collision: collision, behaviour: behaviour)
         
         self.view.addSubview(shapeView)
-        animator = UIDynamicAnimator(referenceView: view)
-        
-        gravity.magnitude = 0.4
-        animator.addBehavior(gravity)
-        collision.addItem(shapeView)
-        collision.translatesReferenceBoundsIntoBoundary = true
         gravity.addItem(shapeView)
-        animator.addBehavior(collision)
+        collision.addItem(shapeView)
+        behaviour.addItem(shapeView)
+        
+        
     }
-    
-
-
-
 }
