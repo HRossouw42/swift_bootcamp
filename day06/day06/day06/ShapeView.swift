dc6de61d15b707b7867565f3c85f4c04e3b69dd2
Harmun <hrossouw@student.wethinkcode.co.za>
2019-10-16 13:08:04 +0200

    random shapes and colour

--- day06/day06/day06/ShapeView.swift
+++ day06/day06/day06/ShapeView.swift
@@ -12,28 +12,68 @@ class ShapeView: UIView {
     
     let size: CGFloat = 150.0
     let lineWidth: CGFloat = 3
+    var fillColor: UIColor!
+    
+    //for random shapes
+    var path: UIBezierPath!
+    
+    func randomColor() -> UIColor {
+        let hue:CGFloat = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+        return UIColor(hue: hue, saturation: 0.8, brightness: 1.0, alpha: 0.8)
+    }
     
     init(origin: CGPoint) {
         super.init(frame: CGRect(x: 0.0, y: 0.0, width: size, height: size))
+        self.path = randomPath()
+        self.fillColor = randomColor()
         self.backgroundColor = UIColor.clear
         self.center = origin
+        
         initGestureRecognizers()
-
     }
     
+    //DRAW FUNCTION
     override func draw(_ rect: CGRect) {
-        let insetRect = rect.insetBy(dx: lineWidth / 2, dy: lineWidth / 2)
-        
-        let path = UIBezierPath(roundedRect: insetRect, cornerRadius: 10)
-        
         UIColor.red.setFill()
+        self.fillColor.setFill()
         path.fill()
+        
                 
         path.lineWidth = self.lineWidth
         UIColor.black.setStroke()
         path.stroke()
     }
     
+    //CHOOSE RANDOM SHAPE
+    func randomPath() -> UIBezierPath {
+
+        let insetRect = self.bounds.insetBy(dx: lineWidth,dy: lineWidth)
+
+        let shapeType = arc4random() % 3
+
+        if shapeType == 0 {
+            return UIBezierPath(roundedRect: insetRect, cornerRadius: 10.0)
+        }
+
+        if shapeType == 1 {
+            return UIBezierPath(ovalIn: insetRect)
+        }
+        return trianglePathInRect(rect: insetRect)
+    }
+    
+    //DRAW TRIANGLE
+    func trianglePathInRect(rect:CGRect) -> UIBezierPath {
+        let path = UIBezierPath()
+
+        path.move(to: CGPoint(x: rect.width / 2.0, y: rect.origin.y))
+        path.addLine(to: CGPoint(x: rect.width,y: rect.height))
+        path.addLine(to: CGPoint(x: rect.origin.x,y: rect.height))
+        path.close()
+
+        return path
+    }
+    
+    //TOUCH FUNCTIONS
     func initGestureRecognizers() {
         let panGR = UIPanGestureRecognizer(target: self, action: #selector(ShapeView.didPan(panGR:)))
         addGestureRecognizer(panGR)
@@ -45,6 +85,7 @@ class ShapeView: UIView {
         addGestureRecognizer(rotationGR)
     }
     
+    //PAN
     @objc func didPan(panGR: UIPanGestureRecognizer) {
 
         self.superview!.bringSubviewToFront(self)
@@ -61,7 +102,7 @@ class ShapeView: UIView {
         panGR.setTranslation(CGPoint.zero, in: self)
     }
     
-    
+    //PINCH
     @objc func didPinch(pinchGR: UIPinchGestureRecognizer) {
 
         self.superview!.bringSubviewToFront(self)
@@ -73,6 +114,7 @@ class ShapeView: UIView {
         pinchGR.scale = 1.0
     }
     
+    //ROTATE
     @objc func didRotate(rotationGR: UIRotationGestureRecognizer) {
 
         self.superview!.bringSubviewToFront(self)
@@ -87,16 +129,5 @@ class ShapeView: UIView {
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+        
 }
